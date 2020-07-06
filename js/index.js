68d10e086ed1b7a917d2129cbbf4fadb24587ee5
Samantha Goodman <slgoodman89@gmail.com>
2020-07-06 15:09:56 -0400

    completed main contact and footer

--- js/index.js
+++ js/index.js
@@ -65,3 +65,38 @@ const startBtn = document.querySelector('.cta-text button')
 startBtn.textContent = siteContent.cta.button
 
 //MAIN CONTENT
+// ******* TOP
+const topContent =  document.querySelectorAll('.top-content .text-content');
+topContent[0].children[0].textContent = siteContent["main-content"]["features-h4"];
+topContent[0].children[1].textContent = siteContent["main-content"]["features-content"];
+topContent[1].children[0].textContent = siteContent["main-content"]["about-h4"];
+topContent[1].children[1].textContent = siteContent["main-content"]["about-content"];
+
+// ********* MIDDLE
+const midImg = document.getElementById('middle-img');
+midImg.src = siteContent["main-content"]["middle-img-src"];
+
+// ********* BOTTOM
+const botContent =  document.querySelectorAll('.bottom-content .text-content');
+
+botContent[0].children[0].textContent = siteContent["main-content"]["services-h4"];
+botContent[0].children[1].textContent = siteContent["main-content"]["services-content"];
+
+botContent[1].children[0].textContent = siteContent["main-content"]["product-h4"];
+botContent[1].children[1].textContent = siteContent["main-content"]["product-content"];
+
+botContent[2].children[0].textContent = siteContent["main-content"]["vision-h4"];
+botContent[2].children[1].textContent = siteContent["main-content"]["vision-content"];
+
+// CONTACT
+const contactHeadline = document.querySelector('.contact h4');
+const contactInfo = document.querySelectorAll('.contact p');
+
+contactHeadline.textContent = siteContent.contact["contact-h4"];
+contactInfo[0].textContent = siteContent.contact.address;
+contactInfo[1].textContent = siteContent.contact.email;
+contactInfo[2].textContent = siteContent.contact.phone;
+
+//FOOTER
+const copyright = document.querySelector('footer p');
+copyright.textContent = siteContent.footer.copyright;
